5ca36c9f4afc73dd8f255cb5cddfc12668ab2eaa
Rick <riccardoguadagnini7@gmail.com>
2024-12-04 09:51:07 +0100

    docker installation

--- programs.sh
+++ programs.sh
@@ -19,14 +19,15 @@ programs_main() {
 docker() {
     welcome "Installing Docker Engine and Docker Compose"
 
-    gum spin --spinner="dot" --title="Setting up the repository" -- sudo dnf -y install dnf-plugins-core
-    sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+    sudo dnf -y install dnf-plugins-core 2>/dev/null
+    gum spin --spinner="dot" --title="Setting up the repository" -- sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 
     gum format "Downloading packages, if prompted this key: 060A 61C5 1B55 8A7F 742B 77AA C52F EB6B 621E 9F35; then accept"
 
     gum spin --spinner="dot" --title="Downloading"-- sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-    gum spin --spinner="dot" --title="Starting & testing Docker installation" -- sudo systemctl enable --now docker
+    gum spin --spinner="dot" --title="Starting Docker" -- sudo systemctl enable --now docker
+    gum spin --spinner="dot" --title="Testing Docker installation" --
     sudo docker run hello-world
 
     gum spin --spinner="dot" --title="Installing docker compose" -- sudo dnf install docker-cli containerd docker-compose docker-switch
